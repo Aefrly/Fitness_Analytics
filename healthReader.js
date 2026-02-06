3f67879c085acd8e40ce10fbf84b20ee99b72ce2
Aefrly <fraley.allison@ic.edu>
2026-02-06 07:53:47 -0600

    Created asynchronous function to read JSON file

---
+++ healthReader.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises');
+
+async function readHealthFile() {
+    try {
+        const data = await fs.readFile('../data/health-metrics.json', 'utf8');
+        const healthData = JSON.parse(data);
+
+        console.log(`Total health entries: ${healthData.length}`);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('Health data file not found.');
+        } else {
+            console.error('Error reading health file:', error.message);
+        }
+    }
+}
+
+readHealthFile();
